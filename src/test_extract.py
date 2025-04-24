8a2e50c9950e5ce3d5fc1a0ccdc2b1610e7dc21f
Heavyymir <richan005@outlook.com>
2025-04-24 00:10:44 -0400

    Exract Markdown functions created, testing file completed

---
+++ src/test_extract.py
@@ -0,0 +1,24 @@
+import unittest
+
+from extract import extract_markdown_images, extract_markdown_links
+
+
+class test_extract(unittest.TestCase):
+	def test_extract_markdown_images(self):
+		matches = extract_markdown_images(
+			"This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+		)	
+		self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+	def test_extract_markdown_links(self):
+		matches = extract_markdown_links(
+			"This is text with a link [to boot dev](https://www.boot.dev) and another [to youtube](https://www.youtube.com/@bootdotdev)"
+		)
+		self.assertListEqual([
+			("to boot dev", "https://www.boot.dev"), 
+			("to youtube", "https://www.youtube.com/@bootdotdev")
+		], matches)
+
+if __name__ == "__main__":
+	unittest.main()
